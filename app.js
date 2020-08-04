58906d34d4331d790460bd91f0803789c3ff422b
Nawodya Jayalath <nawodyahckr@gmail.com>
2020-08-04 19:40:25 +0530

    Remove API Key & Change port to Global

--- app.js
+++ app.js
@@ -60,4 +60,8 @@ app.post('/failure', (req, res) => {
     res.redirect('/');
 })
 
+<<<<<<< HEAD
 app.listen(3000, () => console.log('Server is running on port 3000'));
+=======
+app.listen(process.env.PORT || 3000, () => console.log('Server is running on port 3000'));
+>>>>>>> 0cee01f... change port local to global
